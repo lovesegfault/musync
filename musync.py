386a969de8ac0ac9b1401ff1a528b61cfab7ab7e
Bernardo Meurer <meurerbernardo@gmail.com>
2015-11-20 16:30:09 -0200

    Changed naming style.

--- musync.py
+++ musync.py
@@ -11,172 +11,174 @@ lib = 'C:/Users/berna/Desktop/Lib/'
 dev = 'C:/Users/berna/Desktop/Dev/'
 
 
-# Faster file copying function, arguments go as follows: Source file location,
-# target directory, whether to keep the filename intact and whether to create
+# Faster file copying function, arguments go as follows: source file location,
+# target directory, whether to keep the file_name intact and whether to create
 # the target directory in case it doesn't exist.
-def copy_file(SrcFile, TgtDir, KeepName=True):
-    SourceFile = None
-    TargetFile = None
-    KeepGoing = False
+def copy_file(source_file, target_dir, keep_name=True):
+    source_file = None
+    target_file = None
+    keep_going = False
 
-    # Processes TgtDir depending on filename choice.
-    if KeepName:
-        TgtDir += os.path.basename(SrcFile)
-        print(TgtDir)
+    # Processes target_dir depending on file_name choice.
+    if keep_name:
+        target_dir += os.path.basename(source_file)
+        print(target_dir)
     try:
-        SourceFile = open(SrcFile, 'rb')
-        TargetFile = open(TgtDir, 'wb')
-        KeepGoing = True
+        source_file = open(source_file, 'rb')
+        target_file = open(target_dir, 'wb')
+        keep_going = True
         Count = 0
-        while KeepGoing:
+        while keep_going:
             # Read blocks of size 2**20 = 1048576
-            Buffer = SourceFile.read(2 ** 20)
-            if not Buffer:
+            file_buffer = source_file.read(2 ** 20)
+            if not file_buffer:
                 break
-            TargetFile.write(Buffer)
-            Count += len(Buffer)
+            target_file.write(file_buffer)
+            Count += len(file_buffer)
     finally:
-        if TargetFile:
-            TargetFile.close()
-        if SourceFile:
-            SourceFile.close()
-    return KeepGoing
+        if target_file:
+            target_file.close()
+        if source_file:
+            source_file.close()
+    return keep_going
 
 
 # XXX Ugly workaround-ish, needs improvement.
-# Copies a directory (SrcDir) to TgtDir
-def copy_tree(SrcDir, TgtDir, Replace=True, Repeated=False):
+# Copies a directory (source_dir) to target_dir
+def copy_tree(source_dir, target_dir, Replace=True, repeated=False):
     # Checks if function is being used for self-recursiveness.
-    if not Repeated:
+    if not repeated:
         # If not handles folder naming (check function call to understand this
         # is a weird workaround that just happened to work and I ain't touching
         # it no more.
-        TgtDir = format_dir(TgtDir, os.path.basename(SrcDir.rstrip("/")))
-    if not os.path.isdir(TgtDir):
-        os.makedirs(TgtDir)
-    # Makes Subs as all files and folders in the folder to be copied.
-    Subs = listdir(SrcDir)
-    Errors = []
-    # For every subdirectory/file in Source
-    for Sub in Subs:
-        # Process filenames to remove last "/" and pick Src/Sub and Tgt/Sub
-        SrcName = format_dir(SrcDir, Sub).rstrip("/")
-        TgtName = format_dir(TgtDir, Sub).rstrip("/")
+        target_dir = format_dir(
+            target_dir, os.path.basename(source_dir.rstrip("/")))
+    if not os.path.isdir(target_dir):
+        os.makedirs(target_dir)
+    # Makes subdirs as all files and folders in the folder to be copied.
+    subdirs = listdir(source_dir)
+    errors = []
+    # For every subdirectory/file in source
+    for Sub in subdirs:
+        # Process file_names to remove last "/" and pick Src/Sub and Tgt/Sub
+        source_name = format_dir(source_dir, Sub).rstrip("/")
+        target_name = format_dir(target_dir, Sub).rstrip("/")
         # Using try because file operatins are nasty business.
         try:
             # If it's a dir inside a dir we use this very function to copy it,
             # recursiveness at it's best. Can cause a whole bunch of weird
             # behaviour, don't mess with it too much.
-            if os.path.isdir(SrcName):
-                copy_tree(SrcName, TgtName, Repeated=True)
-            # If it's just a file call copy_file and that's it, KeepName is set
+            if os.path.isdir(source_name):
+                copy_tree(source_name, target_name, repeated=True)
+            # If it's just a file call copy_file and that's it, keep_name is set
             # as false because it's function is being done by the very logic
             # of this for loop.
             else:
-                copy_file(SrcName, TgtName, KeepName=False)
+                copy_file(source_name, target_name, keep_name=False)
         # If things get kinky grab the error and report.
         except (IOError, os.error) as why:
-            Errors.append((SrcName, TgtName, str(why)))
-        if Errors:
-            raise Exception(Errors)
+            errors.append((source_name, target_name, str(why)))
+        if errors:
+            raise Exception(errors)
 
 
-# Checks for new and deleted folders and returns their name.
-def check_folder(SrcDir, TgtDir):
-    # Lists Source and Target folder.
-    Source = listdir(SrcDir)
-    Target = listdir(TgtDir)
+# Checks for new and gone folders and returns their name.
+def check_folder(source_dir, target_dir):
+    # Lists source and target folder.
+    source = listdir(source_dir)
+    target = listdir(target_dir)
     # Then creates a list of deprecated and new directories.
-    Deleted = [FileName for FileName in Target if FileName not in Source]
-    Added = [FileName for FileName in Source if FileName not in Target]
+    gone = [file_name for file_name in target if file_name not in source]
+    new = [file_name for file_name in source if file_name not in target]
     # Returns both lists.
-    return (Added, Deleted)
+    return (new, gone)
 
 
 # Checks for song in case there's a name mismatch or missing file.
-def check_song(SrcFile, TgtDir):
-    Matches = []
+def check_song(source_file, target_dir):
+    matches = []
     # Invariably the new name will be that of the source file, the issue here
     # is finding which song is the correct one.
-    NewName = TgtDir + '/' + os.path.basename(SrcFile)
-    TagSource = FLAC(SrcFile)
+    new_name = target_dir + '/' + os.path.basename(source_file)
+    tag_source = FLAC(source_file)
     # Grabs the number of samples in the original file.
-    SourceSamples = TagSource.info.total_samples
+    source_samples = tag_source.info.total_samples
     # Checks if any song has a matching sample number and if true appends the
-    # song's filename to Matches[]
-    for Song in listdir(TgtDir):
-        SongInfo = FLAC(TgtDir + '/' + Song)
-        if (SongInfo.info.total_samples == SourceSamples):
-            Matches.append(Song)
+    # song's file_name to matches[]
+    for music_file in listdir(target_dir):
+        SongInfo = FLAC(target_dir + '/' + music_file)
+        if (SongInfo.info.total_samples == source_samples):
+            matches.append(music_file)
     # If two songs have the same sample rate (44100Hz for CDs) and the same
-    # length it matches them to the source by filename similarity.
-    if (Matches.count > 1):
-        Diffs = []
-        for Song in Matches:
-            Diffs.append(fuzz.ratio(Song, os.path.basename(SrcFile)))
-        if (max(Diffs) > 0.8):
-            BestMatch = TgtDir + '/' + Matches[Diffs.index(max(Diffs))]
-            os.rename(BestMatch, NewName)
+    # length it matches them to the source by file_name similarity.
+    if (matches.count > 1):
+        deltas = []
+        for music_file in matches:
+            deltas.append(fuzz.ratio(
+                music_file, os.path.basename(source_file)))
+        if (max(deltas) > 0.8):
+            top_match = target_dir + '/' + matches[deltas.index(max(deltas))]
+            os.rename(top_match, new_name)
         else:
-            shutil.copy(SrcFile, TgtDir)
+            shutil.copy(source_file, target_dir)
     # If there's no match at all simply copy over the missing file.
-    elif (Matches.count == 0):
-        shutil.copy(SrcFile, TgtDir)
-    # If a single match is found the filename will be the first item on the
-    # Matches[] list.
+    elif (matches.count == 0):
+        shutil.copy(source_file, target_dir)
+    # If a single match is found the file_name will be the first item on the
+    # matches[] list.
     else:
-        os.rename(TgtDir + '/' + Matches[0], NewName)
+        os.rename(target_dir + '/' + matches[0], new_name)
 
 
 # Syncs folders in a directory and return the change count.
-def sync(SrcDir, TgtDir):
-    AddCount = 0
-    DeleteCount = 0
-    # Grabs the folders to be added and deleted.
-    NewDir, OldDir = check_folder(SrcDir, TgtDir)
+def sync(source_dir, target_dir):
+    add_count = 0
+    del_count = 0
+    # Grabs the folders to be new and gone.
+    new_dir, old_dir = check_folder(source_dir, target_dir)
     # Checks if any and then does add/rm.
-    if OldDir:
-        for Folder in OldDir:
-            shutil.rmtree(TgtDir + Folder)
-            DeleteCount += 1
-    if NewDir:
-        for Folder in NewDir:
-            copy_tree(format_dir(SrcDir, Folder), TgtDir)
-            AddCount += 1
-    return(AddCount, DeleteCount)
+    if old_dir:
+        for directory in old_dir:
+            shutil.rmtree(target_dir + directory)
+            del_count += 1
+    if new_dir:
+        for directory in new_dir:
+            copy_tree(format_dir(source_dir, directory), target_dir)
+            add_count += 1
+    return(add_count, del_count)
 
 
 # Fixes missing metadata fields.
-def fix_metadata(SrcFile, TgtFile):
-    TagSource = FLAC(TgtFile)
-    TagTarget = FLAC(SrcFile)
-    # Checks for deleted tags on source file and deletes them from target.
-    if (set(TagTarget) - set(TagSource)):
-        OldTags = list(set(TagTarget) - set(TagSource))
+def fix_metadata(source_file, target_file):
+    tag_source = FLAC(target_file)
+    tag_target = FLAC(source_file)
+    # Checks for gone tags on source file and deletes them from target.
+    if (set(tag_target) - set(tag_source)):
+        OldTags = list(set(tag_target) - set(tag_source))
         for Tag in OldTags:
             # TODO Right now I haven't quite figured out how to delete
             # specific tags, so workaround is to delete them all.
-            TagTarget.delete()
+            tag_target.delete()
     # Checks for new tags on source file and transfers them to target.
-    if (set(TagSource) != set(TagTarget)):
-        NewTags = list(set(TagSource) - set(TagTarget))
+    if (set(tag_source) != set(tag_target)):
+        NewTags = list(set(tag_source) - set(tag_target))
         for Tag in NewTags:
-            TagTarget["%s" % Tag] = TagSource[Tag]
-            TagTarget.save(TgtFile)
+            tag_target["%s" % Tag] = tag_source[Tag]
+            tag_target.save(target_file)
 
 
 # Does metadata transfer between two files.
-def match_metadata(SrcFile, TgtFile):
-    Altered = 0
-    TagSource = FLAC(SrcFile)
-    TagTarget = FLAC(TgtFile)
+def match_metadata(source_file, target_file):
+    change_count = 0
+    tag_source = FLAC(source_file)
+    tag_target = FLAC(target_file)
     # For every different Tag in source song copy it to target and save.
-    for Tag in TagSource:
-        if TagSource[Tag] != TagTarget[Tag]:
-            Altered += 1
-            TagTarget[Tag] = TagSource[Tag]
-            TagTarget.save(TgtFile)
-    return(Altered)
+    for Tag in tag_source:
+        if tag_source[Tag] != tag_target[Tag]:
+            change_count += 1
+            tag_target[Tag] = tag_source[Tag]
+            tag_target.save(target_file)
+    return(change_count)
 
 
 # Simply does directory formatting to make things easier.
@@ -194,27 +196,30 @@ def format_dir(Main, Second, Third=""):
 
 # Sync main folders in lib with dev.
 sync(lib, dev)
-# For every Artist in lib sync it's Albums
-for Artist in listdir(lib):
-    sync(format_dir(lib, Artist), format_dir(dev, Artist))
-    # For every Album in Artist match songs
-    for Album in listdir(format_dir(lib, Artist)):
-        # Declares lib Album and dev Album to make function calls shorter.
-        CurrentAlbum = format_dir(lib, Artist, Album)
-        CoAlbum = format_dir(dev, Artist, Album)
-        for Song in listdir(CurrentAlbum):
-            if (".flac" in Song or ".FLAC" in Song):
+# For every artist in lib sync it's Albums
+for artist in listdir(lib):
+    sync(format_dir(lib, artist), format_dir(dev, artist))
+    # For every album in artist match songs
+    for album in listdir(format_dir(lib, artist)):
+        # Declares lib album and dev album to make function calls shorter.
+        current_album = format_dir(lib, artist, album)
+        CoAlbum = format_dir(dev, artist, album)
+        for music_file in listdir(current_album):
+            if (".flac" in music_file or ".FLAC" in music_file):
                 try:
                     # Tries to match lib and dev song's metadata.
-                    match_metadata(CurrentAlbum + Song, CoAlbum + Song)
+                    match_metadata(current_album + music_file,
+                                   CoAlbum + music_file)
                 except:
-                    # If that fails will try to fix both Filename and Tag
+                    # If that fails will try to fix both file_name and Tag
                     # fields.
-                    check_song(CurrentAlbum + Song, CoAlbum)
-                    fix_metadata(CurrentAlbum + Song, CoAlbum + Song)
+                    check_song(current_album + music_file, CoAlbum)
+                    fix_metadata(current_album + music_file,
+                                 CoAlbum + music_file)
                     try:
                         # Try again after fix.
-                        match_metadata(CurrentAlbum + Song, CoAlbum + Song)
+                        match_metadata(current_album + music_file,
+                                       CoAlbum + music_file)
                     except Exception as e:
                         # If it still doesn't work there's black magic in place
                         # go sleep, drink a beer and try again later.
