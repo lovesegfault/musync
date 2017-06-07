d7f7b55962963c67e90f8f2d793e1b1b28183eb8
Bernardo Meurer <meurerbernardo@gmail.com>
2017-06-07 23:23:32 +0100

    Implemented sumtype for error

--- src/sync.rs
+++ src/sync.rs
@@ -1,24 +1,41 @@
 // Syncs directories
-use std::io::Result;
+use std::io;
 use std::result;
 use std::path::{Path, PathBuf, StripPrefixError};
 use std::fs;
 use probe;
 
+enum SyncError {
+    IO(io::Error),
+    Prefix(StripPrefixError),
+}
+
+impl From<io::Error> for SyncError {
+    fn from(error: io::Error) -> Self {
+        SyncError::IO(error)
+    }
+}
+
+impl From<StripPrefixError> for SyncError {
+    fn from(error: StripPrefixError) -> Self {
+        SyncError::Prefix(error)
+    }
+}
+
 /// Syncs all files in source to dst.
 /// WARNING: Deletes anything previously in dst
-pub fn copy(src: &Path, dst: &Path, depth: Option<u32>)-> Result<Vec<PathBuf>> {
+pub fn copy(src: &Path, dst: &Path, depth: Option<u32>) -> Result<Vec<PathBuf>, SyncError> {
     clear_dir(dst)?;
     let mut synced_objects: Vec<PathBuf> = Vec::new();
 
     let source_dirs = probe::directories(src, depth)?;
-    for dir in source_dirs{
+    for dir in source_dirs {
         fs::create_dir_all(make_path(src, dir.as_path(), dst)?)?;
         synced_objects.push(dir);
     }
 
     let source_files = probe::files(src, depth)?;
-    for file in source_files{
+    for file in source_files {
         fs::copy(&file, make_path(src, file.as_path(), dst)?)?;
         synced_objects.push(file);
     }
@@ -30,18 +47,18 @@ pub fn copy(src: &Path, dst: &Path, depth: Option<u32>)-> Result<Vec<PathBuf>> {
 
 /// Corrects a path for copying
 /// https://stackoverflow.com/questions/44419890/replacing-path-parts-in-rust
-fn make_path(src: &Path, file: &Path, dst: &Path) -> Result<PathBuf> {
+fn make_path(src: &Path, file: &Path, dst: &Path) -> io::Result<PathBuf> {
     Ok(dst.join(file.strip_prefix(src)?))
 }
 
-fn clear_dir(src: &Path)-> Result<()>{
+fn clear_dir(src: &Path) -> Result<(), SyncError> {
     let file_list = probe::files(src, None)?;
-    for file in file_list{
+    for file in file_list {
         fs::remove_file(file.as_path())?;
     }
     let subdir_list = probe::files(src, None)?;
-    for dir in subdir_list{
+    for dir in subdir_list {
         fs::remove_dir_all(dir.as_path())?;
     }
-Ok(())
+    Ok(())
 }
