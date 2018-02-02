b99e1ee122e306cbce4852757e4fe7a7e5c89478
Bernardo Meurer <meurerbernardo@gmail.com>
2018-02-02 13:34:07 -0800

    Fixed error message
    Former-commit-id: e5ced59289476d912d798831af8ca7878992f718

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::probe_library("mad").expect("---- Failed to detect libmad");
+    pkg_config::probe_library("mad").expect("Failed to detect libmad");
     // TODO: Detect libmagic!
 }
